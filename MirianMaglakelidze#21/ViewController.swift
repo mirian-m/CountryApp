a11c59fab4fc3f7ccea84287dcb9ecdb4c067286
mirian-m <mirianmaglakelidze7@gmail.com>
2022-08-11 10:25:57 +0400

    Initial Commit

---
+++ MirianMaglakelidze#21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MirianMaglakelidze#21
+//
+//  Created by Admin on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
